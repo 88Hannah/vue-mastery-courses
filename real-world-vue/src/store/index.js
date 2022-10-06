25c05754ceeaa44f5cda0fa1c514bf91f0a797b4
88Hannah <hannahsangar@gmail.com>
2022-10-06 19:22:53 +0100

    WIP: Looking into state and getters

--- real-world-vue/src/store/index.js
+++ real-world-vue/src/store/index.js
@@ -4,8 +4,27 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
+  state: {
+    user: { id: 'abc123', name: 'Hannah Sangar'},
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneToDos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: state => {
+      return state.todos.filter(todo => !todo.done).length
+    }
+  },
   mutations: {},
   actions: {},
   modules: {},
