4858acb88ff2bc1f8e3da0368f764b5b3fbfa586
88Hannah <hannahsangar@gmail.com>
2022-10-10 09:13:27 +0100

    Mutations and actions used to add page pagination

--- real-world-vue/src/store/index.js
+++ real-world-vue/src/store/index.js
@@ -8,7 +8,8 @@ export default new Vuex.Store({
   state: {
     user: { id: 'abc123', name: 'Hannah Sangar'},
     categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
-    events: []
+    events: [],
+    totalEvents: 0
   },
   getters: {
     getEventById: state => id => {
@@ -18,12 +19,28 @@ export default new Vuex.Store({
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events){
+      state.events = events
+    },
+    SET_TOTAL(state, totalEvents) {
+      state.totalEvents = totalEvents
     }
   },
   actions: {
     createEvent({ commit }, event) {
       return EventServices.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
+      })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventServices.getEvents(perPage, page)
+      .then(response => {
+        commit('SET_TOTAL', response.headers['x-total-count'])
+        commit('SET_EVENTS', response.data)
+      })
+      .catch(error => {
+        console.log('There was an error: ' + error.response)
       })
     }
   },
--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -4,6 +4,20 @@
 
     <event-card v-for="event in events" :key="event.id" :event="event"/>
 
+    <template v-if="page != 1">
+      <router-link 
+        :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">
+        Previous
+      </router-link>
+    </template>
+
+    <template v-if="isNextPage">
+      <router-link 
+        :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next">
+        Next
+      </router-link>
+    </template>
+
   </div>
 </template>
 
@@ -11,27 +25,35 @@
 
 <script>
 import EventCard from '../components/EventCard.vue'
-import EventServices from '../services/EventServices'
+import { mapState } from 'vuex'
 
 export default {
+
+  data(){
+    return {
+      perPage: 3
+    }
+  },
   
   components: {
     EventCard
   },
-  data() {
-    return {
-      events: []
-    }
-  },
   created() {
-    EventServices.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log('There was an error: ' + error.response)
-      })
+    this.$store.dispatch('fetchEvents', {
+      perPage: this.perPage, 
+      page: this.page
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    isNextPage() {
+      return this.page * this.perPage < this.totalEvents ? true : false
+    },
+    ...mapState(['events', 'totalEvents'])
   }
+
 }
 </script>
 
